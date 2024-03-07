5f06a118e00c325906ae009748398197aadd657c
jgwentworth92 <alphabetboi92@protonmail.com>
2024-03-06 19:34:09 -0500

    test rag application plugin

---
+++ plugins/RagLLM/RagLLM/store_factory.py
@@ -0,0 +1,23 @@
+from langchain_community.embeddings import OpenAIEmbeddings
+
+from .store import AsnyPgVector, ExtendedPgVector
+
+
+def get_vector_store(
+    connection_string: str,
+    embeddings: OpenAIEmbeddings,
+    collection_name: str,
+    mode: str = "sync",
+):
+    if mode == "sync":
+        return ExtendedPgVector(
+            connection_string=connection_string,
+            embedding_function=embeddings,
+            collection_name=collection_name,
+        )
+    elif mode == "async":
+        return AsnyPgVector(
+            connection_string=connection_string,
+            embedding_function=embeddings,
+            collection_name=collection_name,
+        )
---
+++ plugins/RagLLM/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RagLLM",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'fastapi',
+        'pydantic',
+        'langchain',
+        'langchain-community',
+        'langchain-core',
+        'langchain-openai',
+        'SQLAlchemy',
+        'psycopg2-binary',
+        'pgvector'
+    ],
+    entry_points={
+        'my_fastapi_app.plugins': [
+            'RagLLM = RagLLM.routes:add_routes',
+        ],
+    }
+)
